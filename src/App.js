ebc240d9b718062e3b31cc8ec0e04f8223546889
Arthur David <32845935+arthurdlima@users.noreply.github.com>
2020-05-27 19:12:03 -0300

    finishing front-end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Attendant from './components/Attendant';
-
+import Client from './components/Client';
 
 function App() {
 
-    // =============== iniciando state + contador de programa
+    // =============== iniciando state de contador de programa ============
 
     const [programTimer, setProgTimer] = useState(60);
     const intervalRef = useRef();
@@ -19,6 +19,21 @@ function App() {
         }
     });
 
+    // ============== iniciando state de fila =============================
+
+    const [queue, setQueue] = useState([]);
+    const intervalRefQueue = useRef();
+    useEffect(() => {
+        if (programTimer <= 0) {
+            return;
+        } else {
+            const id = setInterval(() => setProgTimer(programTimer - 1), 1000);
+            intervalRefQueue.current = id;
+            return () => clearInterval(intervalRefQueue.current);
+        }
+    });
+
+
 
     function mapClientsToRender() {
 
@@ -31,7 +46,16 @@ function App() {
             <header>
                 <h1>MQMS</h1>
                 <p>Simulation time: {programTimer}s </p>
-                <Attendant />
+                <section className='attendants-section'>
+                    <Attendant />
+                    <Attendant />
+                    <Attendant />
+                </section>
+                <section className='clients-attend-section'>
+                    <Client />
+                    <Client />
+                    <Client />
+                </section>
             </header>
         </div>
     );
---
+++ src/components/Client.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Client() {
+    return (
+        <div className="client">
+            <p>C</p>
+        </div>
+    );
+}
+
+export default Client;
