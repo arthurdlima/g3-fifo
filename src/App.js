9cba851b8be4558644e8819551670da907531493
Arthur David <32845935+arthurdlima@users.noreply.github.com>
2020-06-08 17:52:18 -0300

    adding attendance

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Attendant from './components/Attendant';
 import Client from './components/Client';
 import Empty from './components/Empty';
 
-
+// ===== Os componentes para renderizar =================================
 let renderedAtt;
 let renderedClients;
 
 function App() {
 
-    // =============== iniciando state de contador de programa ============
+    // ============= iniciando state de contador de programa ============
 
     const [programTimer, setProgTimer] = useState(60);
     const intervalRef = useRef();
@@ -26,62 +26,108 @@ function App() {
 
     // ============== iniciando state de fila =============================
 
-    const inicialQueue = {
-        id: 0,
-        qSize: 0,
-        qArray: []
-    };
-
-
-
     const [state, setState] = useState({
-        attendantNum: 4,
-        queueNum: 4,
-        queues: []
+        attendantNum: 3,
+        queueNum: 3,
+        queues: [],
+        inAtt: [],
     });
 
     let tempState = state;
 
     const intervalRefQueue = useRef();
     useEffect(() => {
-
+        //========= verifica se o contador do system chegou a 0 ============
         if (programTimer <= 1) {
             return;
         }
-         //initializing system
+         // ======= inicializa sistema (roda 1 vez)=========================
         if (state.queues == 0) {
             renderedClients = createQueueComponent(state.queueNum);
             renderedAtt = mapAttToComponent(state.attendantNum);
             let increment = 0;
             let q = [];
-            while (increment<=state.queueNum) {
-                let iniQ = inicialQueue;
-                iniQ.id = increment++;
-                q.push(iniQ);
+            while (increment<state.queueNum) {
+                let inicialQueue = {
+                    id: increment,
+                    qSize: 0,
+                    qArray: []
+                };
+                q.push(inicialQueue);
                 increment++;
             }
-
             tempState = { ...tempState, queues: q };
-            //console.log(tempState)
-        } else {
-
-        }
-        const id = setInterval(() => {
             setState({ ...tempState });
-        }, 2000);
 
-        intervalRefQueue.current = id;
-        return () => clearInterval(intervalRefQueue.current);
-    },[state]);
+            return;
+        } else {
+            // === Inicializa fila, atualiza chegada de clientes 2 em 2 segundos ====
 
-    console.log(state);
+            let newState = addToQueue(tempState);
+            renderedClients = mapQueueToComp(newState);
 
+            const id = setInterval(() => {
+                setState({ ...newState });
+            }, 2000);
+            intervalRefQueue.current = id;
+
+            return () => clearInterval(intervalRefQueue.current);
+        }
+
+    }, [state]);
+
+    // =========================== ATENDIMENTO ==============================
+
+    const intervalRefQueue = useRef();
+    useEffect(() => {
+        //========= verifica se o contador do system chegou a 0 ============
+        if (programTimer <= 1) {
+            return;
+        }
+        // ======= inicializa sistema (roda 1 vez)=========================
+        if (state.queues == 0) {
+            renderedClients = createQueueComponent(state.queueNum);
+            renderedAtt = mapAttToComponent(state.attendantNum);
+            let increment = 0;
+            let q = [];
+            while (increment < state.queueNum) {
+                let inicialQueue = {
+                    id: increment,
+                    qSize: 0,
+                    qArray: []
+                };
+                q.push(inicialQueue);
+                increment++;
+            }
+            tempState = { ...tempState, queues: q };
+            setState({ ...tempState });
+
+            return;
+        } else {
+            // === Inicializa fila, atualiza chegada de clientes 2 em 2 segundos ====
+
+            let newState = addToQueue(tempState);
+            renderedClients = mapQueueToComp(newState);
+
+            const id = setInterval(() => {
+                setState({ ...newState });
+            }, 2000);
+            intervalRefQueue.current = id;
+
+            return () => clearInterval(intervalRefQueue.current);
+        }
+
+    }, [state]);
+
+
+
+
+    // ===== Roda 1 vez (inicio de programa) e após atualizar o n de atendentes ====
     function mapAttToComponent(attendantNum) {
         let AttCompArray = [];
         for (let i = 0; i < attendantNum; i++) {
             AttCompArray.push(<div key={i}>< Attendant /></div>);
         }
-        //console.log(AttCompArray);
         return AttCompArray;
     }
         /*
@@ -98,18 +144,70 @@ function App() {
         let queueCompArray = [];
         for (let i = 0; i < queueNum; i++) {
             queueCompArray.push(
-
                 <section key={i} className='clients-queue-section'>
-                    <Client />
                 </section>);
         }
-
-        //console.log(queueCompArray)
         return queueCompArray; 
     }
 
-    function manageQueue() {
+    function mapQueueToComp(newState) {
 
+        let queueCompArray = [];
+        let tempArray = [];
+        let clients = [];
+        for (let i = 0; i < newState.queueNum; i++) {
+            for (let j = 0; j < newState.queues[i].qSize; j++) {
+                clients.push(<Client />); 
+            }
+            tempArray.push(
+                <section className='clients-queue-section'>
+                    {clients}
+                </section>
+            );
+            queueCompArray[i] = tempArray;
+            tempArray = [];
+            clients = [];
+        }
+        console.log(queueCompArray);
+        return queueCompArray;
+    }
+
+    function addToQueue(curState) {
+        //console.log('Estado cur ->', curState)
+
+        let q = curState.queues;
+        
+        let id = 0;
+        let prevSize = q[0].qSize;
+        let currSize = q[0].qSize;
+        // ======== verificando a menor fila ====================
+        for (let i = 0; i < curState.queueNum; i++) {
+            currSize = q[i].qSize;
+            if (currSize == prevSize && prevSize == 0) {
+                id = q[i].id;
+                break;
+            } else if (currSize > prevSize) {
+                continue;
+            } else if (currSize < prevSize) {
+                prevSize = currSize;
+                id = q[i].id;
+                continue;
+            }
+        }
+        // ========= adicionando na menor fila ==================
+        
+        const updatedQ = curState.queues.map(s => {
+            if (s.id == id) {
+                s.qSize++;
+                s.qArray.push(1);
+            }
+            return s;
+        }); 
+        
+        const updatedState = { ...curState, queues: updatedQ};
+
+        return updatedState;
+    
     }
    
 
